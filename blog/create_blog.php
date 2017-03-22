da97d854340a84b53d8ff2b3bdd5ab559a54c0b7
Jonas <Zatnosk@users.noreply.github.com>
2017-03-22 10:13:56 +0100

    bugfix

--- blog/create_blog.php
+++ blog/create_blog.php
@@ -2,7 +2,7 @@
 require_once __DIR__."/../access.php";
 Access::logged_in();
 require_once __DIR__."/blog.php";
-if(isset($_POST['title']) && isset($_POST['content'])){
+if(!empty($_POST['title']) && !empty($_POST['content'])){
 	$blog_id = Blog::write($_POST['title'], $_POST['content'], $_POST['visibility']);
 	header('Location: '.ROOTPATH.'blog/?id='.$blog_id);
 	exit;
