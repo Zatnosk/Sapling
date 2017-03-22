53456dbce0a5db99e40b92ae3ff4d1e42bca5660
Jonas <Zatnosk@users.noreply.github.com>
2017-03-22 10:15:32 +0100

    bugfix

--- forum/create_post.php
+++ forum/create_post.php
@@ -3,7 +3,7 @@ require_once __DIR__."/../access.php";
 Access::logged_in();
 require_once __DIR__."/../data.php";
 require_once __DIR__."/../person/person.php";
-if(isset($_GET['thread']) && isset($_POST['content'])){
+if(isset($_GET['thread']) && !empty($_POST['content'])){
 	$thread = intval($_GET['thread']);
 	if($thread) ForumData::write_post($thread, Person::get_current(), $_POST['content']);
 }
